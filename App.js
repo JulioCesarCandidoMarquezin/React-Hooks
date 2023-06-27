c455fceb60e0146a318e4ac7f2b18ba42a139af0
JulioCesarCandidoMarquezin <juliocesarmarquezin@gmail.com>
2023-06-27 16:49:27 -0400

    Commit inicial

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+state = {
+    repositories: []
+}
+
+export default function App(){
+    const [repositories, setRepositories] = useState([
+        { id: 1, name: 'repo-1'},
+        { id: 2, name: 'repo-2'},
+        { id: 3, name: 'repo-3'},
+    ]);
+
+    function handleAddRepository() {
+        setRepositories([
+            ...repositories,
+            { id: Math.random(), name: "novo-repo"}
+        ])
+    }
+
+    return(
+        <>
+        <ul>
+            {repositories.map(repo => (
+                <li key={repo.id}>{repo.name}</li>
+            ))}
+        </ul>
+        <button onClick={handleAddRepository}>Adicionar repositório</button>
+        </>
+    )
+}
